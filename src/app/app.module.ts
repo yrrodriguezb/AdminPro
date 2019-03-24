36d36386a06499f505bcc1dff69fb591f1ceec2b
Yeison Rodriguez <yrrodriguezb@gmail.com>
2019-03-24 16:59:55 -0500

    Modificaciones en el proyecto por modulos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,36 +4,22 @@ import { NgModule } from '@angular/core';
 import { APP_ROUTES } from './app.routes';
 
 import { AppRoutingModule } from './app-routing.module';
+import { PagesModule } from './pages/pages.module';
+
 import { AppComponent } from './app.component';
 import { LoginComponent } from './login/login.component';
-import { DashboardComponent } from './pages/dashboard/dashboard.component';
-import { ProgressComponent } from './pages/progress/progress.component';
-import { GraphicsComponent } from './pages/graphics/graphics.component';
-import { HeaderComponent } from './shared/header/header.component';
-import { SidebarComponent } from './shared/sidebar/sidebar.component';
-import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
-import { NotfoundComponent } from './shared/notfound/notfound.component';
-import { PagesComponent } from './pages/pages.component';
 import { RegisterComponent } from './login/register.component';
-
 
 @NgModule({
   declarations: [
     AppComponent,
     LoginComponent,
-    DashboardComponent,
-    ProgressComponent,
-    GraphicsComponent,
-    HeaderComponent,
-    SidebarComponent,
-    BreadcrumbsComponent,
-    NotfoundComponent,
-    PagesComponent,
     RegisterComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
+    PagesModule,
     APP_ROUTES
   ],
   providers: [],
